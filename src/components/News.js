033ca63a6db8f819c50cf7ae3ea7265fc78e201c
shivang-stack <shivang169401@gmail.com>
2021-10-30 13:33:36 +0530

    ui changes

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ function News(props) {
 
   return (
     <div>
-      <h1 className="head__text">NewsForYou👋</h1>
+      <h1 className="head__text">NewsForYou<span role ="img">👋</span></h1>
       <div className="all__news">
         {data
           ? data.articles.map((news) => (
